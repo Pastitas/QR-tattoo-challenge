09181ca17a8b6e18580f572e70011611cc136281
Pastitas <Pastitas@users.noreply.github.com>
2020-08-20 02:14:18 +0200

    Update tattooCipher.py

--- tattooCipher.py
+++ tattooCipher.py
@@ -8,46 +8,29 @@ message = raw_input('Enter your message: ')
 for symbol in message:
 
     if symbol.isalpha():
-
         num = ord(symbol)
-
         num += i
-
-
-
+        
         if symbol.isupper():
-
             if num > ord('Z'):
                 num -= 26
-
             elif num < ord('A'):
-
                 num += 26
 
         elif symbol.islower():
-
             if num > ord('z'):
-
                 num -= 26
-
             elif num < ord('a'):
-
                 num += 26
-
-
 
         translated += chr(num)
 
     else:
-
         translated += symbol
-
     if (i >= 26):
-
         i = 0
-
+        
     else:
-
         i = i + 1
 
 print('Your translated text is: ' + translated)
